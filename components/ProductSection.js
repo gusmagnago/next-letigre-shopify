6d765e144a19da6ea86b4e355bcf0093dc8abff5
Gustavo <gusmagnago@gmail.com>
2021-07-15 17:09:18 +0100

    productSection component re-corrected

--- components/ProductSection.js
+++ components/ProductSection.js
@@ -3,7 +3,7 @@ import ProductDetails from "@/components/ProductDetails";
 
 function ProductSection({ productData }) {
   return (
-    <div className="md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 flex flex-col items-center justify-center w-11/12 h-screen max-w-6xl mx-auto space-y-8">
+    <div className="md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 flex flex-col items-center justify-center w-11/12 max-w-6xl min-h-screen mx-auto space-y-8">
       <ProductImage images={productData.images.edges} />
       <ProductDetails productData={productData} />
     </div>
